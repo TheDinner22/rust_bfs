1d0cda91835d6d6995d2d52313d51440a34715f4
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-08-19 22:19:12 -0400

    commiting to test something with clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
+//! # common breadth first search (bfs) logic
+//! Find the most efficent path from point a to point b in some array or vec or struct representing
+//! space
+//!
+//! ## how it works
+//!
+//! todo!()
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn dummy_implementation_of_array_as_grid() {
+        let mut grid = vec![];
+        for i in 0..100 {
+            grid.push(i)
+        }
+
+        let x = Some(5);
+        let y = x.unwrap();
     }
 }
