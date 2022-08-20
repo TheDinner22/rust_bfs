6b09e5446934fcd0a7f8863568dc08d6f1efc51a
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-08-19 23:07:32 -0400

    added sudo code and ideas on how to describe behavior

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,26 @@
 //!
 //! todo!()
 
+pub trait LocationAware {
+    type Cell;
 
+    fn get_current_location(){todo!()}
+
+    fn get_all_moves_from_current_location();
+    fn take_all_moves();
+    fn is_target_cell(cell_to_check: &Self::Cell) -> bool;
+}
+
+pub trait Bfs {
+    type Cell;
+
+    fn bfs(){}
+}
+
+// LocationAware implies breadth first search
+impl<T: LocationAware> Bfs for T {
+    type Cell = T::Cell;
+}
 
 
 #[cfg(test)]
@@ -17,8 +36,21 @@ mod tests {
         for i in 0..100 {
             grid.push(i)
         }
-
-        let x = Some(5);
-        let y = x.unwrap();
     }
 }
+
+// some ideas and sudo code
+//
+// need an array or array like struct or something (must somehow, loose-ly represent some space, 2d, 3d, etc.)
+// (must have some smallest denominator of space i.e
+// a block in minecraft, a square in chess, or a cell in conways game of life)
+// that implements a BFS trait and a location trait
+// location trait: somehow keep track of position in the space being represented and be able to
+// move through that space (moving is a seperate trait??) 
+// BFS trait: where you can call the trait to get all the possible moves from your current "location," 
+// then spawn several locations (how can i spawn a trait) that do all possible moves and repeat
+// until reaching some specified location the path to that location is the path of "least
+// resistance" and is returned or something
+//
+//
+//
