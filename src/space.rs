c94ea035b6ed412a75d465eb0893f2d982a6e19f
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-08-28 16:25:02 -0400

    tested Space trait

--- src/space.rs
+++ src/space.rs
@@ -9,13 +9,52 @@ pub trait RepresentsSpace {
     fn get_id_from_cell (&self, cell: &Self::Cell) -> Self::CellId {
         cell.get_uid()
     }
-
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::uid::HasId;
+
+    use super::RepresentsSpace;
+
+    struct Maze {
+        cells: Vec<Cell>
+    }
+
+    impl Default for Maze {
+        fn default() -> Self {
+            Maze { cells: (0..20).into_iter().map(|n| Cell { id: n}).collect() }
+        }
+    }
+
+    impl RepresentsSpace for Maze {
+        type CellId = i32;
+
+        type Cell = Cell;
+
+        fn get_cell_from_id(&self, cell_id: Self::CellId) -> &Self::Cell {
+            &self.cells[cell_id as usize]
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct Cell {
+        id: i32
+    }
+
+    impl HasId for Cell {
+        type ID= i32;
+
+        fn get_uid(&self) -> Self::ID {
+            self.id
+        }
+    }
+
     #[test]
-    fn it_works(){
-        assert_eq!(1+1, 2);
+    fn create_maze_and_compare_ids(){
+        let m = Maze::default();
+
+        assert_ne!(m.get_id_from_cell(&m.cells[0]), m.get_id_from_cell(&m.cells[1]));
+        assert_eq!(m.get_cell_from_id(0), &m.cells[0])
     }
 }
