4030775fc768ff4d904561bf9bc8f811ad280611
joe <not_real_email@gmail.com>
2022-08-28 11:39:55 -0400

    fixed typos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// this is for testing only making sure remote wo&rks
+// this is for testing only making sure remote works
 // use rust_bfs::*;
 
 #[derive(Default)]
@@ -16,7 +16,6 @@ struct Grid {
 // 0  1  2  3  4  5
 // 6  7  8  9  10 11
 // 12 13 14 15 16 17
-//
 impl Default for Grid {
     fn default() -> Self {
         let cells = (0..18)
