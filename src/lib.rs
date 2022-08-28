60eb94385a928f12c021d3b982225134566703c1
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-08-28 14:43:48 -0400

    sort of implemented the PathAware trait

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,22 @@ where
     }
 }
 
+pub trait PathAware {
+    type Cell: uid::IsUnique;
+    type Move: Copy;
+
+    fn get_paths(&self) -> &Vec<Path<Self::Cell, Self::Move>>;
+    fn set_paths(&mut self, new_paths: Vec<Path<Self::Cell, Self::Move>>);
+    fn remove_path_by_index(&mut self, path_index: usize);
+
+    fn remove_paths_by_indexes(&mut self, indexes_to_remove: Vec<usize>){
+        indexes_to_remove
+            .into_iter()
+            .enumerate()
+            .for_each(|(i, r)| { self.remove_path_by_index(r - i) });
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
