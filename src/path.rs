7d50f34be635889d5d8398217900458e6e3496c9
joe goodman <jgood1133@gmail.com>
2022-08-28 20:26:31 -0400

    made it so the path.rs file's tests compile and pass

--- src/path.rs
+++ src/path.rs
@@ -26,54 +26,54 @@ mod path_struct {
         }
     }
 
-    // #[cfg(test)]
-    // mod tests {
-    //     use super::*;
-    //
-    //     // vars needed by multiple tests
-    //     #[derive(Clone, Copy, PartialEq, Debug)]
-    //     enum ExampleMove {
-    //         Up,
-    //     }
-    //
-    //     #[derive(Debug, PartialEq)]
-    //     struct ExampleCell {
-    //         id: i32
-    //     }
-    //
-    //     #[test]
-    //     fn create_path() {
-    //         let random_cell = ExampleCell { id: 22 };
-    //
-    //         let p: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![])
-    //         );
-    //
-    //         assert_eq!(p.moves_taken, vec![]);
-    //         assert_eq!(p.start_cell, &ExampleCell{ id: 22});
-    //     }
-    //
-    //     #[test]
-    //     fn clone_path(){
-    //         let random_cell = ExampleCell { id: 345 };
-    //
-    //         let p: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![])
-    //         );
-    //
-    //         let p_with_move_up: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![ExampleMove::Up])
-    //         );
-    //
-    //         let new_p = p.clone_and_append(ExampleMove::Up);
-    //
-    //         assert_eq!(new_p, p_with_move_up);
-    //         assert_ne!(p, new_p);
-    //     }
-    // }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        // vars needed by multiple tests
+        #[derive(Clone, Copy, PartialEq, Debug)]
+        enum ExampleMove {
+            Up,
+        }
+
+        #[derive(Debug, PartialEq)]
+        struct ExampleCell {
+            id: i32
+        }
+
+        #[test]
+        fn create_path() {
+            let _random_cell = ExampleCell { id: 22 };
+
+            let p: Path<i32, ExampleMove> = Path::new(
+                22,
+                Some(vec![])
+            );
+
+            assert_eq!(p.moves_taken, vec![]);
+            assert_eq!(p.start_cell_id, 22);
+        }
+
+        #[test]
+        fn clone_path(){
+            let _random_cell = ExampleCell { id: 345 };
+
+            let p: Path<i32, ExampleMove> = Path::new(
+                345,
+                Some(vec![])
+            );
+
+            let p_with_move_up: Path<i32, ExampleMove> = Path::new(
+                345,
+                Some(vec![ExampleMove::Up])
+            );
+
+            let new_p = p.clone_and_append(ExampleMove::Up);
+
+            assert_eq!(new_p, p_with_move_up);
+            assert_ne!(p, new_p);
+        }
+    }
 }
 
 mod bfs_struct;
