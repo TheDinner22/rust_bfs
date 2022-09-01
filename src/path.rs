2008be192d4d473c3e563648f989b13d8084c3d4
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-09-01 17:55:35 -0400

    removed weird merge comment

--- src/path.rs
+++ src/path.rs
@@ -42,58 +42,6 @@ mod path_struct {
                 .collect()
         }
     }
-
-<<<<<<< HEAD
-    // fix these next!!!!
-    // #[cfg(test)]
-    // mod tests {
-    //     use super::*;
-    //
-    //     // vars needed by multiple tests
-    //     #[derive(Clone, Copy, PartialEq, Debug)]
-    //     enum ExampleMove {
-    //         Up,
-    //     }
-    //
-    //     #[derive(Debug, PartialEq)]
-    //     struct ExampleCell {
-    //         id: i32
-    //     }
-    //
-    //     #[test]
-    //     fn create_path() {
-    //         let random_cell = ExampleCell { id: 22 };
-    //
-    //         let p: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![])
-    //         );
-    //
-    //         assert_eq!(p.moves_taken, vec![]);
-    //         assert_eq!(p.start_cell, &ExampleCell{ id: 22});
-    //     }
-    //
-    //     #[test]
-    //     fn clone_path(){
-    //         let random_cell = ExampleCell { id: 345 };
-    //
-    //         let p: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![])
-    //         );
-    //
-    //         let p_with_move_up: Path<ExampleCell, ExampleMove> = Path::new(
-    //             &random_cell,
-    //             Some(vec![ExampleMove::Up])
-    //         );
-    //
-    //         let new_p = p.clone_and_append(ExampleMove::Up);
-    //
-    //         assert_eq!(new_p, p_with_move_up);
-    //         assert_ne!(p, new_p);
-    //     }
-    // }
-=======
     #[cfg(test)]
     mod tests {
         use super::*;
@@ -142,7 +90,7 @@ mod path_struct {
             assert_ne!(p, new_p);
         }
     }
->>>>>>> bbc982c70f7f9a88b956fe1fc71907962377ebd5
 }
 
 pub mod bfs_struct;
+
